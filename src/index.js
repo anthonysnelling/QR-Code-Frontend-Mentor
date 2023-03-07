0dbf945c03fce931da99aaf7e519cdb385ca9fd8
Anthony <anthony.snelling@live.com>
2023-03-06 22:27:06 -0500

    Qr Code project from Frontend Mentor

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import "./styles.css"
+import QR from '../images/image-qr-code.png'
+
+document.title = "QR CODE Frontend Mentor"
+
+const qrCode = new Image();
+qrCode.src =  QR;
+qrCode.classList += 'qrCode';
+
+let intro = document.createElement("h1");
+intro.textContent = "Improve your front-end skills by building projects"
+intro.classList += 'cardHeading'
+
+let intro2 = document.createElement("p");
+intro2.textContent = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level"
+intro2.classList += 'attribution'
+
+const cardContainer = document.createElement("div");
+cardContainer.classList += "cardContainer"
+
+let card  = document.createElement("div");
+card.classList += 'card'
+
+let newline = document.createElement("br");
+
+document.body.appendChild(cardContainer);
+cardContainer.appendChild(card)
+card.appendChild(qrCode);
+card.appendChild(intro);
+card.appendChild(newline);
+card.appendChild(intro2);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: "./src/index.js",
+    // print: "./src/print.js",
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // webpack has a built in asset loader that you can use
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+    ],
+  },
+};
